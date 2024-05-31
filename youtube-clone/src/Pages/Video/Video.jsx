cf51a713de271668909ddefc9685ecb3c37b37d3
Kushal Khandhara <158416966+kushalkhandhara@users.noreply.github.com>
2024-06-01 05:15:55 +0530

    Update Video.jsx->Api Key Removed

--- youtube-clone/src/Pages/Video/Video.jsx
+++ youtube-clone/src/Pages/Video/Video.jsx
@@ -3,7 +3,7 @@ import {useParams} from "react-router-dom"
 import "./Video.css"
 import PlayVideo from "../../components/PlayVideo/PlayVideo"
 import Recommended from "../../components/Recommended/Recommended"
-// AIzaSyBN1VsOi5vX3FiItVxXgUXxIrTdbU8zmSI
+
 export default function Video() {
 
   const {videoId,categoryId} = useParams();
